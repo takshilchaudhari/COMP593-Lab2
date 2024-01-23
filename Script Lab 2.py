bb2ab521e89c3e66c8d2ae605d31671049d656c6
Takshil Chaudhari <156624213+takshilchaudhari@users.noreply.github.com>
2024-01-23 09:55:06 -0500

    step 1,2,3 completed
    made directory, accessed it.

---
+++ Script Lab 2.py
@@ -0,0 +1,55 @@
+def  main():
+
+    about_me = {
+
+      'full_name' : 'Takshil Chaudhari',
+      'student_id': 10323590,
+      'pizza_toppings': ['ONIONS','PANEER','JALEPINOS'],
+      'movie_list': [
+          
+          {
+              'title': 'Dunki',
+              'genre': 'comedy/drama',
+          },
+          {
+              'title': 'Salaar',
+              'genre': 'drama/action',
+          }
+      ]
+
+    }
+
+    # TODO: Step 3 - Add another movie to the data structure
+
+    movie_list={ 
+           'title':'the gaazi attack',
+           'genre':'action',
+       }
+    
+    about_me['movie_list'].append{movie_list}
+         add_pizza_toppings[about_me,pizza_toppings]
+
+# TODO: Step 4 - Function that prints student name and ID	
+    
+    def print_student_name_and_id(about_me):
+         print(f"my name is {about_me[full_name]},but you can call me{about_me[full_name].split{}[0]}.")
+         return
+    
+# TODO: Step 5 - Function that adds pizza toppings to data structure
+def add_pizza_toppings(about_me, toppings):
+    return
+
+# TODO: Step 6 - Function that prints bullet list of pizza toppings
+def print_pizza_toppings(about_me):
+    return
+
+# TODO: Step 7 - Function that prints comma-separated list of movie genres
+def print_movie_genres(about_me):
+    return 
+
+# TODO: Step 8 - Function that prints comma-separated list of movie titles
+def print_movie_titles(movie_list):
+    return
+    
+if __name__ == '__main__':
+    main()
